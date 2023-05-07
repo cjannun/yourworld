f9d44808ef5e90e88056b946ceb3801feb4268e6
cjannun <93235878+cjannun@users.noreply.github.com>
2023-05-07 14:32:03 -0400

    Started customization form

--- your-world/src/App.js
+++ your-world/src/App.js
@@ -5,18 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className="customization-form">
+          <label> select character
+            <input type="radio"/>
+          </label>
+
+          <label> select background
+            <input type="radio"/>
+          </label>
+
+          <label> select weather
+            <input type="radio"/>
+          </label>
+
+          <input type="submit" value="update world"/>
+        </form>
       </header>
     </div>
   );
